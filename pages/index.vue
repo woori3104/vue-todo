bd9c8b96b4c7e06be6e104b1c92e77e68b853e9c
woori <woori.kim.se@gmail.com>
2023-08-27 16:52:08 +0900

    nuxt-todo

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Todo App</h1>
+    <input v-model="newTodo" @keyup.enter="addTodo" placeholder="New Todo" />
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        {{ todo }}
+        <button @click="removeTodo(index)">Delete</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator';
+
+@Component
+export default class TodoApp extends Vue {
+  newTodo: string = '';
+  todos: string[] = [];
+
+  addTodo() {
+    if (this.newTodo.trim() !== '') {
+      this.todos.push(this.newTodo.trim());
+      this.newTodo = '';
+    }
+  }
+
+  removeTodo(index: number) {
+    this.todos.splice(index, 1);
+  }
+}
+</script>
